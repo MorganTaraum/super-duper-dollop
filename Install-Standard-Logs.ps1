34a28c264f755cfbbb50df377241f4c53b3c2aea
MorganTaraum <139836081+MorganTaraum@users.noreply.github.com>
2026-05-17 17:44:55 +0800

    Create Install-Standard-Logs.ps1

---
+++ Install-Standard-Logs.ps1
@@ -0,0 +1,29 @@
+# Configure Better Event Log from Yamato 
+iwr https://raw.githubusercontent.com/Yamato-Security/EnableWindowsLogSettings/refs/heads/main/YamatoSecurityConfigureWinEventLogs.bat -outfile $env:Temp\YamatoSecurityConfigureWinEventLogs.bat
+
+& $env:Temp\YamatoSecurityConfigureWinEventLogs.bat
+
+Remove-Item -Path "$env:Temp\YamatoSecurityConfigureWinEventLogs.bat"
+
+
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPL-3.0
+
+# Enable PowerShell Logging
+$regConfig = @"
+regKey,name,value,type
+"HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging","EnableScriptBlockLogging",1,"DWORD"
+"HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging","EnableScriptBlockInvocationLogging",1,"DWORD"
+"HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging","EnableModuleLogging",1,"DWORD"
+"HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames",*,*,"String"
+"@
+
+Write-host "Setting up PowerShell registry settings.."
+$regConfig | ConvertFrom-Csv | ForEach-Object {
+    if(!(Test-Path $_.regKey)){
+        Write-Host $_.regKey " does not exist.."
+        New-Item $_.regKey -Force
+    }
+    Write-Host "Setting " $_.regKey
+    New-ItemProperty -Path $_.regKey -Name $_.name -Value $_.value -PropertyType $_.type -force
+}
